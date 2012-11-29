0a2adfa8c740848b0942d692b4ea34cd204ff711
Tassilo Horn <tsdh@gnu.org>
2012-11-29 20:35:11 +0100

    add all primitive types; also do array type specs

--- testfiles/class-with-method.java
+++ testfiles/class-with-method.java
@@ -1,5 +1,5 @@
 public final abstract class Foo<Bar> extends Xyz implements IBaz, IBla<X> {
-    public final int foo(int x) {
-	return 1 + x;
+    public final double[] foo(int x) {
+	return new double[] {1.0 + x, 3.3};
     }
 }
