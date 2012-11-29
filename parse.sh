70fcc0555a24a1b665e720c3c140725c9ee8dcb1
Tassilo Horn <tsdh@gnu.org>
2012-11-29 16:30:26 +0100

    minchang to parse.sh script

--- parse.sh
+++ parse.sh
@@ -1,5 +1,2 @@
-#!/bin/zsh
-
 java -cp ../edl/lib/strategoxt.jar:../jgralab/build/jar/jgralab.jar:../edl/build/jar/edl.jar:build/jar/mini-java-parser.jar de.uni_koblenz.simple_java.builder.JavaGraphBuilder $*
 
-echo "Fini."
