7d61152d87932176da4e41439577a54f0a7cde7b
Tassilo Horn <tsdh@gnu.org>
2012-11-30 12:40:46 +0100

    Allow enum consts like FOO(1, 23)

--- testfiles/src/testfiles/Bar.java
+++ testfiles/src/testfiles/Bar.java
@@ -4,8 +4,20 @@ public interface Bar extends Comparable<Bar> {
 
 	enum ABC {
 		A, B, C;
-		
+
 		public void foo() {
+		}
+	}
+
+	enum ComplexEnum {
+		X(1, 2), Y(2, 3), Z(3, 4);
+		
+		int i;
+		int j;
+
+		ComplexEnum(int i, int j) {
+			this.i = i;
+			this.j = i;
 		}
 	}
 
