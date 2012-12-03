70973f97cf94ec3c1f935ece8e35fe940e14d181
Tassilo Horn <tsdh@gnu.org>
2012-12-03 15:34:23 +0100

    allow optional ; after class/enum/iface/anno body

--- parse.sh
+++ parse.sh
@@ -1,2 +1,2 @@
-java -cp ../edl/lib/strategoxt.jar:../jgralab/build/jar/jgralab.jar:../edl/build/jar/edl.jar:build/jar/mini-java-parser.jar de.uni_koblenz.simple_java.builder.JavaGraphBuilder $*
+java -Xss4M -cp ../edl/lib/strategoxt.jar:../jgralab/build/jar/jgralab.jar:../edl/build/jar/edl.jar:build/jar/mini-java-parser.jar de.uni_koblenz.simple_java.builder.JavaGraphBuilder $*
 
