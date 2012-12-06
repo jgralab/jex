69adb38c8d3f3d36d1d3bb7fe79f75bb37c22ac0
Tassilo Horn <tsdh@gnu.org>
2012-12-06 10:35:49 +0100

    fix a bug in type resolution; better errors; create annotation elements

--- testfiles/src/testfiles/Bar.java
+++ testfiles/src/testfiles/Bar.java
@@ -1,49 +1,54 @@
 package testfiles;
 
 public interface Bar extends Comparable<Bar> {
+        
+        @interface TestAnnotation {
+                String desc() default "description";
+                String[] keywords();
+        }
 
-	enum ABC {
-		A, B, C;
+        enum ABC {
+                A, B, C;
 
-		public void foo() {
-		}
-	}
+                public void foo() {
+                }
+        }
 
-	enum ComplexEnum {
-		X(1, 2), Y(2, 3), Z(3, 4);
+        enum ComplexEnum {
+                X(1, 2), Y(2, 3), Z(3, 4);
 
-		int i;
-		int j;
+                int i;
+                int j;
 
-		ComplexEnum(int i, int j) {
-			this.i = i;
-			this.j = i;
-		}
-	}
+                ComplexEnum(int i, int j) {
+                        this.i = i;
+                        this.j = i;
+                }
+        }
 
-	public static class Box<T> {
-		private T boxedItem;
+        public static class Box<T> {
+                private T boxedItem;
 
-		private Box(T x) {
-			boxedItem = x;
-		}
+                private Box(T x) {
+                        boxedItem = x;
+                }
 
-		public static <T> Box<T> makeBox(T x) {
-			return new Box<T>(x);
-		}
+                public static <T> Box<T> makeBox(T x) {
+                        return new Box<T>(x);
+                }
 
-		public void set(T t) {
-			this.boxedItem = t;
-		}
+                public void set(T t) {
+                        this.boxedItem = t;
+                }
 
-		public T get() {
-			return boxedItem;
-		}
-	}
+                public T get() {
+                        return boxedItem;
+                }
+        }
 
-	public int[] gimme(int a, long b);
+        public int[] gimme(int a, long b);
 
-	public enum EnumAsSingleton {
-		THE
-	}
+        public enum EnumAsSingleton {
+                THE
+        }
 }
