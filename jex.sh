36e8262be84f485ab3594a215f3ddff2f352595f
Tassilo Horn <tsdh@gnu.org>
2012-12-14 15:18:34 +0100

    rename to jex

---
+++ jex.sh
@@ -0,0 +1,2 @@
+java -Xmx1G -cp build/jar/jex.jar:../jgralab/build/jar/jgralab.jar:../edl/build/jar/edl.jar:../edl/lib/strategoxt.jar de.uni_koblenz.jgralab.jex.builder.JavaGraphBuilder $*
+
--- parse.sh
+++
@@ -1,2 +0,0 @@
-java -Xmx1G -cp testfiles/bin:../edl/lib/strategoxt.jar:../jgralab/build/jar/jgralab.jar:../edl/build/jar/edl.jar:build/jar/mini-java-parser.jar de.uni_koblenz.simple_java.builder.JavaGraphBuilder $*
-
